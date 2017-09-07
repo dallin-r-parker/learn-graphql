811cd6b83a466b0fa99627aeae1fe2a154a58893
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-07 14:50:15 -0400

    set up json-server for dummy data and have second queryType

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql')
-const { users } = require('./Users')
-const _ = require('lodash')
+const axios = require('axios')
 
 const {
 	GraphQLObjectType,
@@ -9,12 +8,29 @@ const {
 	GraphQLSchema
 } = graphql
 
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString},
+		name: { type: GraphQLString},
+		description: { type: GraphQLString}
+	}
+})
+
 const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
-		age: { type: GraphQLInt }
+		age: { type: GraphQLInt },
+		company: { 
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(res => res.data)
+				//console.log('parentValue: ', parentValue, "args: ", args)
+			}
+		}
 	}
 })
 
@@ -25,8 +41,17 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: {id: {type: GraphQLString}},
 			resolve(parentValue, args) {
-				console.log('args: ', args)
-				return _.find(users, {id: args.id})
+				console.log('args: ', args, 'parentValue: ', parentValue)
+				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(res => res.data)
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: {id: {type: GraphQLString}},
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+					.then(resp => resp.data)
 			}
 		}
 	}
